51b156b39d480d4ad1ae1ce1e409f37439d3d5a3
abhinavmishra12 <77190638+abhinavmishra12@users.noreply.github.com>
2021-02-27 17:32:18 +0530

    Add files via upload
    java file

---
+++ dt3.java
@@ -0,0 +1,26 @@
+//Methods of LocalDate
+//minusDays(int)
+//plusDays(int)
+//wap to display todays date yesterdate and tommorow date
+package dt;
+import java.time.*;
+
+public class dt3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate ob=LocalDate.now();
+		System.out.println("today's date is :"+ob);
+		LocalDate ob1= ob.minusDays(1);
+		System.out.println("yesterday date is :"+ob1);
+		LocalDate ob2=ob1.plusDays(2);
+		System.out.println("tommorow date is :"+ob2);
+		
+		/*System.out.println("today's date:"+ob);
+	    System.out.println("yesterday's date"+ob.minusDays(1));
+	    System.out.println("tomorrow's date"+ob.plusDays(1));*/
+	
+	}
+	
+
+}
